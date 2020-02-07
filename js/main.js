398427471efcf690aa1901beecaa667752606bea
Danila Kudashov <48285878+kudashovdanila@users.noreply.github.com>
2020-02-07 16:55:58 +0700

    Загрузил весь сайт

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: '.arrow',
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    }
+  }
+});
+
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header__content');
+menuButton.addEventListener('click', function () {
+  menu.classList.toggle('header-active');
+  menuButton.classList.toggle('menu-button-active');
+});
